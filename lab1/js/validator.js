f71819903a5d88418f2fdbe9049092a60364a314
Romariok <ronya-ko@yandex.ru>
2023-09-27 00:14:18 +0300

    final version

--- lab1/js/validator.js
+++ lab1/js/validator.js
@@ -1,75 +1,12 @@
-let x, y, r;
-
-
-document.getElementById("submit").onclick = function () {
-   if (validateR && validateX && validateY) {
-      fetch("script.php", {
-         method: "GET",
-         headers: { "Content-type": "application/x-www-form-urlencoded; charset=UTF-8" },
-      }).then(response => response.text()).then(function (serverAnswer) {
-         setPointer();
-         document.getElementById("outputContainer").innerHTML = serverAnswer;
-      }).catch(err => createNotification("Ошибка HTTP. Повторите попытку позже." + err));
-   }
-
-};
+let r;
+let valX = false, valY = false, valR = false;
 
 
 
+let graph_values = document.getElementsByClassName("graph_value");
 
-function validateR() {
-   if (isNumeric(r)) return true;
-   else {
-      createNotification("r не выбран");
-      return false;
-   }
-}
 
-function validateY() {
-   y = document.querySelector("input[name=textY]").value.replace(",", ".");
-   if (y == undefined) {
-      createNotification("Y не введён");
-      return false;
-   }
-   else if ((!isNumeric(y))) {
-      createNotification("Y - не число ");
-      return false;
-   }
-   else if (!(y >= -3 && y <= 3)) {
-      createNotification("Y - не входит в диапозон");
-      return false;
-   }
-   else return true;
-}
 
-function validateX() {
-   try {
-      var checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
-      if (checkboxes.length > 1) {
-         createNotification("Выберите только одно значение");
-         return false;
-      }
-      else {
-         x = checkboxes[0];
-         return true;
-      }
-   }
-   catch (err) {
-      createNotification("X не выбрано");
-   }
-
-}
-
-function isNumeric(n) {
-   return !isNaN(parseFloat(n)) && isFinite(n);
-}
-
-function setPointer() {
-   let pointer = document.getElementById("pointer");
-   pointer.style.visibility = "visible";
-   pointer.setAttribute("cx", x * 54 + 150); // 1:54 - масштаб, +150 позволяет вести отсёт от 0,0
-   pointer.setAttribute("cy", y * 54 + 150);
-}
 
 function createNotification(message) {
    let outputContainer = document.getElementById("outputContainer");
@@ -84,4 +21,246 @@ function createNotification(message) {
       let span = document.querySelector(".notification");
       span.textContent = message;
    }
+}
+
+
+function formatParams(params) {
+   return (
+      "?" +
+      Object.keys(params)
+         .map(function (key) {
+            return key + "=" + encodeURIComponent(params[key]);
+         })
+         .join("&")
+   );
+}
+
+
+
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+checkboxes.forEach((checkbox) => {
+   checkbox.addEventListener('click', () => {
+      checkboxes.forEach((cb) => {
+         if (cb !== checkbox) {
+            cb.checked = false;
+         }
+      });
+   });
+});
+
+function validateR(rr) {
+   if ((!isNumeric(rr))) {
+      createNotification("R - не число ");
+      valR = false;
+      return;
+   }
+   else if (!((rr >= 0) && (rr <= 5))) {
+      createNotification("R - не входит в диапозон");
+      valR = false;
+      return;
+   }
+   else {
+      valR = true;
+      return rr;
+   }
+}
+
+function validateY(yy) {
+   if (yy === undefined) {
+      createNotification("Y не введён");
+      valY = false;
+      return;
+   }
+   else if ((!isNumeric(yy))) {
+      createNotification("Y - не число ");
+      valY = false;
+      return;
+   }
+   else if (!((yy >= -3) && (yy <= 3))) {
+      createNotification("Y - не входит в диапозон");
+      valY = false;
+      return;
+   }
+   else {
+      valY = true;
+      return yy;
+   }
+}
+
+function validateX(xx) {
+   if ((!isNumeric(xx))) {
+      createNotification("X - не число ");
+      valX = false;
+      return;
+   }
+   else if (!((xx >= -4) && (xx <= 4))) {
+      createNotification("X - не входит в диапозон");
+      valX = false;
+      return;
+   }
+   else {
+      valX = true;
+      return xx;
+   }
+
+
+}
+
+
+
+
+function isNumeric(n) {
+   return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function setPointer(x, y, r) {
+   let pointer = document.getElementById("pointer");
+   pointer.style.visibility = "visible";
+   pointer.setAttribute("cx", (x * 120) / r + 150);
+   pointer.setAttribute("cy", 150 - (y * 120) / r);
+}
+
+function saveData(response) {
+   localStorage.setItem('data', response);
+}
+
+const tbody = document.querySelector(".main__table tbody");
+
+const form = document.querySelector(".form");
+form.addEventListener("submit", (e) => {
+   e.preventDefault(); // prevent submitting
+
+   let params = {
+      x: validateX(document.querySelector("input[type=checkbox]:checked").value),
+      y: validateY(document.querySelector("input[name=textY]").value.replace(",", ".")),
+      r: validateR(r),
+   };
+   if (valR && valX && valY) {
+      const target = "php/script.php" + formatParams(params);
+
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", target);
+
+      xhr.onloadend = () => {
+         if (xhr.status === 200) {
+            tbody.innerHTML = xhr.response;
+            saveData(xhr.response);
+            setPointer(document.querySelector("input[type=checkbox]:checked").value, document.querySelector("input[name=textY]").value.replace(",", "."), r);
+            createNotification("Результат обработаны");
+         } else
+            console.log(
+               "status: ",
+               xhr.status,
+               "X: ",
+               x,
+               "Y: ",
+               y,
+               "R: ",
+               r
+            );
+      };
+
+      xhr.send();
+   }
+
+});
+
+
+
+
+const clearBtn = document.querySelector('.reset_button[type="reset"]');
+clearBtn.addEventListener("click", (e) => {
+   e.preventDefault();
+
+   let xhr = new XMLHttpRequest();
+   xhr.onloadend = () => {
+      if (xhr.status === 200) {
+         tbody.innerHTML = "";
+         localStorage.removeItem('data');
+         createNotification("Таблица очищена");
+      } else console.log("status: ", xhr.status);
+   };
+   xhr.open("POST", "php/clear.php");
+   xhr.send();
+});
+
+const buttons = document.querySelectorAll('.button-R');
+buttons.forEach((g) => {
+   g.addEventListener("click", (e) => {
+      let element = e.target;
+      r = `${element.innerText}`;
+      for (let index = 0; index < graph_values.length; index++) {
+         graph_values[index].textContent = r;
+      }
+   });
+});
+
+var getUrlParameter = function getUrlParameter(sParam) {
+   var sPageURL = window.location.search.substring(1),
+      sURLVariables = sPageURL.split('&'),
+      sParameterName,
+      i;
+   for (i = 0; i < sURLVariables.length; i++) {
+      sParameterName = sURLVariables[i].split('=');
+      if (sParameterName[0] === sParam) {
+         return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+      }
+   }
+   return false;
+};
+
+
+window.onload = function () {
+   try {
+      var savedData = localStorage.getItem('data');
+
+      tbody.innerHTML = savedData;
+
+
+      if (isNumeric(getUrlParameter('x')) && isNumeric(getUrlParameter('y')) && isNumeric(getUrlParameter('r'))) {
+         let params = {
+            x: validateX(getUrlParameter('x')),
+            y: validateY(getUrlParameter('y')),
+            r: validateR(getUrlParameter('r')),
+         };
+         if (valR && valX && valY) {
+            const target = "php/script.php" + formatParams(params);
+
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", target);
+
+            xhr.onloadend = () => {
+               if (xhr.status === 200) {
+                  tbody.innerHTML = xhr.response;
+                  saveData(xhr.response);
+                  setPointer(params['x'], params['y'], params['r']);
+                  createNotification("Результат обработаны");
+               } else
+                  console.log(
+                     "X: ",
+                     params['x'],
+                     "Y: ",
+                     params['y'],
+                     "R: ",
+                     params['r']
+                  );
+            };
+
+            xhr.send();
+         }
+      }
+   }
+   catch (err) {
+      console.log(
+         err,
+         "X: ",
+         getUrlParameter('x'),
+         "Y: ",
+         getUrlParameter('y'),
+         "R: ",
+         getUrlParameter('r')
+      );
+   }
+
+
 }
--- lab1/php/script.php
+++ lab1/php/script.php
@@ -1,13 +1,25 @@
 <?php
 
+$x = isset($_GET['x']) ? floatval($_GET['x']) : null;
+$y = isset($_GET['y']) ? floatval($_GET['y']) : null;
+$r = isset($_GET['r']) ? floatval($_GET['r']) : null;
 
 
 @session_start();
 
-date_default_timezone_SET($_GET["timezone"]);
-$x = (float) $_GET["x"];
-$y = (float) $_GET["y"];
-$r = (float) $_GET["r"];
+date_default_timezone_set('Europe/Moscow');
+$current_time = date("H:i:s");
+if (!checkData($x, $y, $r)) {
+   http_response_code(412);
+   echo ("x={$x}, y={$y}, r={$r}");
+   return;
+}
+
+$exec_time = round(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 5) . ' мс';
+$result = checkCoordinates($x, $y, $r);
+
+$_SESSION['tdata'][] = [$x, $y, $r, $current_time, $exec_time, $result];
+
 
 
 
@@ -19,11 +31,25 @@ function checkData($x, $y, $r)
 function checkCoordinates($x, $y, $r)
 {
    if (
-      ($x <= 0 && sqrt(($x * $x) + ($y * $y)) <= $r / 2) && ($y >= 0 && $y <= $r / 2) || ($x >= 0 && $x <= $r + $y) && ($y <= 0 && $y <= -$r) ||
+      ($x <= 0 && sqrt(pow($x, 2) + pow($y, 2)) <= $r / 2) && ($y >= 0 && $y <= $r / 2) || ($x >= 0 && $x <= $r - (-$y)) && ($y <= 0 && $y <= -$r) ||
       ($x >= 0 && $x <= $r) && ($y >= 0 && $y <= $r / 2)
    )
       return "Yes";
    else
       return "No";
 }
+
+foreach ($_SESSION["tdata"] as $rdata) {
+   echo <<<HTML
+   <tr>
+       <td>$rdata[0]</td>
+       <td>$rdata[1]</td>
+       <td>$rdata[2]</td>
+       <td>$rdata[3]</td>
+       <td>$rdata[4]</td>
+       <td>$rdata[5]</td>
+   </tr>
+HTML;
+}
+
 ?>
